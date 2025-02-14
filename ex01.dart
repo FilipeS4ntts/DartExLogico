c358b84e90cba22c095e3818cd680e92c65a848b
Filipe Santana <filipesantanawrk@gmail.com>
2025-02-14 08:51:33 -0300

    upd

--- ex01.dart
+++ ex01.dart
@@ -1,11 +1,15 @@
-/*a. Faça um programa que recebe o salário de um colaborador e o reajuste segundo o
+/*
+a.Faça um programa que recebe o salário de um colaborador e o reajuste segundo o
 seguinte critério, baseado no salário atual;(Entrada será fixa) por enquanto
 Critérios:
-b. Salários até R$ 1080,00 (incluindo): aumento de 20%;
-c. Salários entre R$ 1080,00 e R$1700,00: aumento de 15%;
-d. Salários entre R$ 1700,00 e R$ 2000,00: aumento de 10%;
-e. Salários de R$ 2000,00 em diante: aumento de 5%
-Após o aumento ser realizado; informe na tela;*/
+
+  b. Salários até R$ 1080,00 (incluindo): aumento de 20%;
+  c. Salários entre R$ 1080,00 e R$1700,00: aumento de 15%;
+  d. Salários entre R$ 1700,00 e R$ 2000,00: aumento de 10%;
+  e. Salários de R$ 2000,00 em diante: aumento de 5%
+  --Após o aumento ser realizado; informe na tela--
+*/
+  
   
    int percentual = 0;
    int salario = 2500;
@@ -28,8 +32,4 @@ Após o aumento ser realizado; informe na tela;*/
   else if(salario >= 1700){
     percentual = 5;
     print('Salários de 2000,00 em diante aumento de 5%: ${(salario * percentual) / 100}');
-    
-  
-
   }
-  

26d2e3cf5dc918858cc4a70d8370eb92037fbea6
Filipe Santana <filipesantanawrk@gmail.com>
2025-02-14 08:58:04 -0300

    changes

---
+++ ex02.dart
@@ -0,0 +1,7 @@
+/*
+A partir do preço à vista de um determinado produto, calcule o preço total a pagar e o
+valor da prestação mensal, referentes ao pagamento parcelado. Se o pagamento for
+parcelado em 3 vezes deve ser dado um acréscimo de 10% no total a ser pago. Se o
+parcelamento for em 5 vezes, o acréscimo será de 20%. Lembrando que são apenas as 2
+opções para parcelamento.
+*/
